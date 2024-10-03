b21918cb91d4b68ead652edb352acf2546663a12
cormochamelion <cormocodran1.5@gmail.com>
2024-10-03 11:47:43 +0200

    manifest: Add shiny as dependency

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,5 +1,9 @@
-;; What follows is a "manifest" equivalent to the command line you gave.
-;; You can store it in a file that you may then pass to any 'guix' command
-;; that accepts a '--manifest' (or '-m') option.
+(define %packages (map specification->package
+                       (list "r-shiny")))
 
-(specifications->manifest (list "r" "r-devtools" "r-usethis"))
+(define %dev-packages (map specification->package
+                           (list "r"
+                                 "r-devtools"
+                                 "r-usethis")))
+
+(packages->manifest (append %dev-packages %packages))
