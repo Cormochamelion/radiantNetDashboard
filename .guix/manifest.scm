e299c2d10c6a9c998d9f74e1eeacbddc1d359238
cormochamelion <cormocodran1.5@gmail.com>
2024-10-03 13:39:55 +0200

    Add initial packages

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,5 +1,15 @@
 (define %packages (map specification->package
-                       (list "r-shiny")))
+                       (list "r-data-table"
+                             "r-dt"
+                             "r-dbi"
+                             "r-dbplyr"
+                             "r-dplyr"
+                             "r-import"
+                             "r-lubridate"
+                             "r-magrittr"
+                             "r-shiny"
+                             "r-rsqlite"
+                             "r-withr")))
 
 (define %dev-packages (map specification->package
                            (list "r"
