78f9bc53c1bf07605b6f474b784066cf6a30b1f0
cormochamelion <cormocodran1.5@gmail.com>
2024-10-03 13:40:18 +0200

    Add initial app

---
+++ R/dashboard.R
@@ -0,0 +1,24 @@
+import::here("DT", "DTOutput", "renderDT")
+import::here(
+  "shiny",
+  "dateInput",
+  "fluidPage",
+  "shinyApp"
+)
+
+import::here(
+  "database.R", "get_raw_data_df_date", .character_only = TRUE, .directory = "R"
+)
+
+dashboard_ui <- fluidPage(
+  dateInput("raw_data_date", "Daily output"),
+  DTOutput("daily_raw_df")
+)
+
+dashboard_server <- function(input, output) {
+  output$daily_raw_df <- renderDT(get_raw_data_df_date(input$raw_data_date))
+}
+
+radiantNetDashboard <- function(...) {
+  shinyApp(dashboard_ui, dashboard_server, ...)
+}
