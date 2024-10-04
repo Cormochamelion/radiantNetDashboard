698932a36223a1f9048c6fbf35992437981d0890
cormochamelion <cormocodran1.5@gmail.com>
2024-10-04 14:53:09 +0200

    run_app: Add port

--- dev/run_app.sh
+++ dev/run_app.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-APP_COMMAND="shiny::runApp(radiantNetDashboard::radiantNetDashboard())"
+PORT="${SHINY_PORT:-6542}"
+APP_COMMAND="shiny::runApp(radiantNetDashboard::radiantNetDashboard(), port = $PORT)"
 R_COMMAND="Rscript --vanilla -e"
 
 if command -v guix &> /dev/null
