6813b3aa176dd72f871b745f316ec3e58cba7611
cormochamelion <cormocodran1.5@gmail.com>
2024-11-26 01:49:17 +0100

    helper-global: Use import
      For better transparency.

--- tests/testthat/helper-global.R
+++ tests/testthat/helper-global.R
@@ -1,5 +1,6 @@
-library("DBI")
-library("withr")
+import::here("DBI", "dbConnect", "dbDisconnect")
+import::here("RSQLite", "SQLite")
+import::here("withr", "defer")
 
 local_gen_db_conn <- function(env = parent.frame()) {
   db_path <- test_path("fixtures/generation_and_usage.sqlite3")
