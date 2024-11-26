d75243949708bb29c0401f6591dc2c28d7405c36
cormochamelion <cormocodran1.5@gmail.com>
2024-11-26 19:29:34 +0100

    package: Declare NSE names as global variables

---
+++ R/radiantNetDashboard-package.R
@@ -0,0 +1,7 @@
+# Names used in non-standard evaluation contexts, declared here so that
+# `devtools::check()` doesn't complain about that.
+utils::globalVariables(
+  c(
+    ".", "time", "hour", "minute", "wattage_type", "wattage", "StateOfCharge"
+  )
+)
