b11726b6a98daf4cf1bb81ca5ed438e2c47f7d02
cormochamelion <cormocodran1.5@gmail.com>
2025-02-09 22:39:39 +0100

    channels.scm: Update guix
      To include radian for ease of development.

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -3,7 +3,7 @@
        (url "https://git.savannah.gnu.org/git/guix.git")
        (branch "master")
        (commit
-        "b522b468cbefcae6170e260ffadadb57f6f5ca54")
+        "e27e63e6fe72d3a6cb6a8755f290ec710d339a9a")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
