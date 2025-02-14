e844bdbd4efab8604356fae02e9066859fc86c4d
cormochamelion <cormocodran1.5@gmail.com>
2025-02-14 15:03:56 +0100

    test-dashboard: Add file with basic test

---
+++ tests/testthat/test-dashboard.R
@@ -0,0 +1,13 @@
+import::here("shinytest2", "AppDriver")
+
+skip_if_not(
+  "radiantNetDashboard" %in% installed.packages(),
+  "radiantNetDashboard needs to be properly installed for shinytest2."
+)
+
+test_that(
+  "the dashboard can be started without issue",
+  {
+    expect_no_error(AppDriver$new(radiant_net_dashboard()))
+  }
+)
