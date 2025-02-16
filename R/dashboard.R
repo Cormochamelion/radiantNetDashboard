b1625e7bdb13c12575098619f10a4c63a27fecb6
cormochamelion <cormocodran1.5@gmail.com>
2025-02-16 23:56:12 +0100

    dashboard: Add layout with aggregated data

--- R/dashboard.R
+++ R/dashboard.R
@@ -37,9 +37,9 @@ report_panel <- function(title, db_pool, id_list) {
 #' Generate a list containing server and UI components for the dashboard bundled
 #' with a connection pool to the app's database.
 #'
-#' @importFrom bslib nav_panel navset_pill_list page_navbar
+#' @importFrom bslib page_navbar
 #' @importFrom pool dbPool poolClose
-#' @importFrom shiny fluidPage onStop
+#' @importFrom shiny onStop
 #' @importFrom RSQLite SQLite
 get_app_with_pool <- function() {
   db_pool <- dbPool(
@@ -52,18 +52,23 @@ get_app_with_pool <- function() {
   list(
     server = function(input, output) {
       dailyProductionServer("daily_production", db_pool)
+      aggregatedProductionServer("monthly_production", db_pool, "month")
+      aggregatedProductionServer("yearly_production", db_pool, "year")
+      aggregatedProductionServer("total_production", db_pool, "total")
+      dailyProductionServer("daily_production", db_pool)
     },
     ui = page_navbar(
       title = "RadiantNetDashboard",
       bg = "#4d4d4d",
       inverse = TRUE,
-      nav_panel(
+      report_panel(
         "Production/Consumption",
-        navset_pill_list(
-          nav_panel(
-            "Daily", fluidPage(dailyProductionUI("daily_production", db_pool))
-          ),
-          widths = c(2, 10)
+        db_pool,
+        list(
+          "daily" = "daily_production",
+          "monthly" = "monthly_production",
+          "yearly" = "yearly_production",
+          "total" = "total_production"
         )
       )
     )
