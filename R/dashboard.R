4e3654fa3390a8a353f8d83bf632839508da37cd
cormochamelion <cormocodran1.5@gmail.com>
2025-02-16 23:54:51 +0100

    dashboard: Add fun showing nav panel
      Adding aggregated displays.

--- R/dashboard.R
+++ R/dashboard.R
@@ -1,3 +1,39 @@
+#' Display a navigation panel containing reports of daily, monthly, yearly,
+#' and total consumption/production data.
+#'
+#' @param title Title of the entire navigation panel.
+#' @param db_pool Pool of connections to the generation & usage database.
+#' @param id_list Named list of ids connecting the UIs of each timeframe (daily,
+#'  monthly, etc.) to their respective servers.
+#'
+#' @importFrom bslib nav_panel navset_pill_list
+#' @importFrom shiny fluidPage
+report_panel <- function(title, db_pool, id_list) {
+  stopifnot(!is.null(names(id_list)))
+
+  nav_panel(
+    title,
+    navset_pill_list(
+      nav_panel(
+        "Daily", fluidPage(dailyProductionUI(id_list$daily, db_pool))
+      ),
+      nav_panel(
+        "Monthly",
+        fluidPage(aggregatedProductionUI(id_list$monthly, db_pool, "month"))
+      ),
+      nav_panel(
+        "Yearly",
+        fluidPage(aggregatedProductionUI(id_list$yearly, db_pool, "year"))
+      ),
+      nav_panel(
+        "Total",
+        fluidPage(aggregatedProductionUI(id_list$total, db_pool, "total"))
+      ),
+      widths = c(2, 10)
+    )
+  )
+}
+
 #' Generate a list containing server and UI components for the dashboard bundled
 #' with a connection pool to the app's database.
 #'
